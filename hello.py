00b882d08392a66a21bd1a22f0db88a658b3c176
key-forr <denyskifor@gmail.com>
2025-06-18 21:28:12 +0300

    first

--- hello.py
+++ hello.py
@@ -1,1 +1,15 @@
-print("Hello, world")
+name: str = input("What's your name? ").strip().title()
+
+# name = name.capitalize()
+# name = name.title()
+
+# name = name.strip().title()
+
+print(f"Hello, {name}")
+
+'''
+print("Hello,", name, sep="     insert    ")
+print("Hello, " + name, end=" | ")
+print(f"Hello, {name}")
+print("Hello, \"friend\"")
+'''
