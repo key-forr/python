3070994b5fbb714fde10cb1be883ca178d7eaac9
key-forr <denyskifor@gmail.com>
2025-06-20 22:38:20 +0300

    match

--- house.py
+++ house.py
@@ -8,3 +8,20 @@ match name:
     case _:
         print("Berlin")
     
+
+age: int = int(input("How old are you? "))
+
+person: dict = {
+    "name": name,
+    "age": age
+}
+
+match person:
+    case { "name": "Denys", "age": age} if age < 18:
+        print("Cool")
+    case {"name": "Denys", "age": 24} if age == 23:
+        print("It's difficult")
+    case {"name": "Denys", "age": 24} if age == 24:
+        print("It's so difficult")
+    case _:
+        print('I don\'t know')
