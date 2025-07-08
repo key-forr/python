739a9b434f889d3f2d5c47ab645dfd22bc29aa88
key-forr <denyskifor@gmail.com>
2025-07-08 23:15:41 +0300

    harvard-course/libraties

--- generate.py
+++ generate.py
@@ -1,5 +1,9 @@
-from random import choice
+import random
 
-coin = choice(["heads", "tails"])
+my_list: list = [1,2,3,4,5,6,7,8,9]
 
-print(coin)
+coin: str = random.choice(["heads", "tails"])
+value: int = random.randint(0, 100)
+random.shuffle(my_list)
+
+print(coin, value, my_list, sep="\n")
