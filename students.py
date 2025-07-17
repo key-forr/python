40181f869040073d0aefd07b363760e2740ae045
key-forr <denyskifor@gmail.com>
2025-07-17 20:54:53 +0300

    write ti csv file

--- students.py
+++ students.py
@@ -1,17 +1,10 @@
 import csv
 
-students = []
+name = input("What's your name? ")
+home = input("Where's your home? ")
+phone = input("What's your phone number? ")
 
 
-with open("students.csv") as file:
-    reader = csv.DictReader(file)
-    for row in reader:
-        students.append({"name": row["name"], "house": row["house"], "phone": row["phone"]})
-
-
-def get_name(student):
-    return student["name"]
-
-
-for student in sorted(students, key=get_name):
-    print(f"{student['name']} is in {student['house']}, phone number is {student['phone']}")
+with open("students.csv", "a") as file:
+    write = csv.writer(file)
+    write.writerow([name, home, phone])
