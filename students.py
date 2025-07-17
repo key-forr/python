f603fdea932951dff57492ed00abc247443afaf7
key-forr <denyskifor@gmail.com>
2025-07-17 15:16:17 +0300

    sort dictionary with func

--- students.py
+++ students.py
@@ -1,9 +1,15 @@
 students = []
 
+
 with open("students.csv") as file:
     for line in file:
         name, house, phone = line.rstrip().split(",")
         students.append({"name": name, "house": house, "phone": phone})
 
-for student in sorted(students, key=lambda student: student["phone"]):
+
+def get_name(student):
+    return student["name"]
+
+
+for student in sorted(students, key=get_name):
     print(f"{student['name']} is in {student['house']}, phone number is {student['phone']}")
