933cee022f79eec4fa6913719ca392ac007ef39a
key-forr <denyskifor@gmail.com>
2025-07-17 15:15:30 +0300

    sort dictionary with lambda

--- students.py
+++ students.py
@@ -1,4 +1,9 @@
+students = []
+
 with open("students.csv") as file:
     for line in file:
-        name, city, phone = line.rstrip().split(",")
-        print(f"{name} is in {city}, phone number: {phone}")
+        name, house, phone = line.rstrip().split(",")
+        students.append({"name": name, "house": house, "phone": phone})
+
+for student in sorted(students, key=lambda student: student["phone"]):
+    print(f"{student['name']} is in {student['house']}, phone number is {student['phone']}")
