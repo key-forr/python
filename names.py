9a72bf8f5cad0ef9cab57d621ef1dc82d2e1ab0f
key-forr <denyskifor@gmail.com>
2025-07-17 13:54:05 +0300

    write in file

--- names.py
+++ names.py
@@ -1,6 +1,4 @@
 name: str = input("What's your name? ")
 
-#file = open("names.txt", "w")
-file = open("names.txt", "a")
-file.write(f"{name}\n")
-file.close()
+with open("names.txt", "a") as file:
+    file.write(f"{name}\n")
