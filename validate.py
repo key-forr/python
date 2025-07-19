2afa0973c714a3407447cd092241975b3b90b1c5
key-forr <denyskifor@gmail.com>
2025-07-19 19:04:17 +0300

    validate email

--- validate.py
+++ validate.py
@@ -1,6 +1,23 @@
-email = input("What's your email address? ").strip()
+email = input("What's your enail address? ").strip()
 
-if "@" in email:
-    print("Valid")
-else:
-    print("Unvalid")
+def validate_email(email):
+    if "@" not in email or email.count("@") != 1:
+        return "Invalid"
+    
+    name, domain = email.split("@")
+
+    if not name:
+        return "Invalid"
+    
+    if "." not in domain:
+        return "Invalid"
+    
+    parts = domain.rsplit(".", 1)
+    if len(parts) != 2 or not parts[0] or not parts[1]:
+        return "Invalid"
+    
+
+    return "Valid"
+
+result = validate_email(email)
+print(result)
