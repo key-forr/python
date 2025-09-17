2612a089da8412ad070f75f3a40ffb6197324a06
key-forr <denyskifor@gmail.com>
2025-09-17 18:31:40 +0300

    beautiful conclusion

--- ports-scanner.py
+++ ports-scanner.py
@@ -3,6 +3,7 @@ import termcolor
 
 
 def scan(target, ports):
+    print('\n' + 'Starting Scan For ' + str(target))
     for port in range(1, ports):
         scan_port(target, port)
 
@@ -20,7 +21,7 @@ targets = input("[*] Enter Targets to Scan(split them by ,): ")
 ports = int(input("[*] Enter How Many Ports You Want to Scan: "))
 
 if ',' in targets:
-    print("[*] Scanning Multiple Targets")
+    print(termcolor.colored("[*] Scanning Multiple Targets", 'green'))
 
     for ip_address in targets.split(','):
         scan(ip_address.strip(' '), ports)
